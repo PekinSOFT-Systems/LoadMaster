5fd6eba6cd8394e1967ef39fa7fff3d6a89baf31
Sean Carrick <sean@pekinsoft.com>
2020-10-06 19:48:56 -0500

    Update to BrokerPage
    I have updated the `BrokerPage` to allow the user to enter a new broker/agent to the database by selecting the `brokerList` entry, "Add New Broker/Agent..."
    
    By allowing the user to do this, they will not need to use the `NewBrokerDlg` or `Brokers` internal frame prior to running the load booking wizard.
    
    Also, I am working on adding a summary page as the last page of the wizard. By providing this page, the user will be able to review the data entered and go back if corrections need to be made. With this page added, the `StopsPage` will be set to `WizardController.MODE_CAN_CONTINUE` and the summary page will be set to `WizardController.MODE_CAN_FINISH`. 
    
    _NOTE_: I thought about making the `StopsPage` allow finishing the wizard, but believe that we should show the summary and let the user finish from there. If you have other ideas, please let me know. This is not something fixed in stone, so we can discuss it freely.
    
    As this is a weeknight, and I have to make a few deliveries tomorrow, I am going to stop here. I will pick up tomorrow with creating the entries in the `loads.tbl` and `stops.tbl` data files. Once this is done, the Book New Load Wizard will be complete and work can finally continue. As of right now, we (I) am running 7 days behind schedule for the first release.
    
    -SC
    Signed-off-by:Sean Carrick <sean@pekinsoft.com>

--- Load Master/src/com/pekinsoft/loadmaster/view/Holder.java
+++ Load Master/src/com/pekinsoft/loadmaster/view/Holder.java
@@ -33,14 +33,6 @@
  */
 package com.pekinsoft.loadmaster.view;
 
-import com.pekinsoft.loadmaster.Starter;
-import com.pekinsoft.loadmaster.err.InvalidTimeException;
-import com.pekinsoft.loadmaster.model.CustomerModel;
-import com.pekinsoft.loadmaster.model.StopModel;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import javax.swing.table.DefaultTableModel;
-
 /**
  *
  * @author Sean Carrick
@@ -63,62 +55,29 @@ public class Holder extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel4 = new javax.swing.JLabel();
-
-        jLabel4.setText("jLabel4");
+        jScrollPane1 = new javax.swing.JScrollPane();
+        summaryEditor = new javax.swing.JEditorPane();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jScrollPane1.setViewportView(summaryEditor);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 404, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 624, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 342, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 391, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Holder.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Holder.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Holder.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Holder.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Holder().setVisible(true);
-            }
-        });
-    }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel4;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JEditorPane summaryEditor;
     // End of variables declaration//GEN-END:variables
 }
--- Load Master/src/com/pekinsoft/loadmaster/view/wiz/book/BrokerPage.java
+++ Load Master/src/com/pekinsoft/loadmaster/view/wiz/book/BrokerPage.java
@@ -28,7 +28,10 @@
  *  
  *  WHEN          BY                  REASON
  *  ------------  ------------------- ------------------------------------------
- *  Sep 6, 2020  Sean Carrick        Initial creation.
+ *  Sep 6, 2020   Sean Carrick        Initial creation.
+ *  Oct 6, 2020   Sean Carrick        Added "Add New Broker/Agent..." item to 
+ *                                    the brokerList JCombobox and the method to
+ *                                    display the Add Broker/Agent dialog.
  * *****************************************************************************
  */
 
@@ -177,6 +180,7 @@ public class BrokerPage extends WizardPage {
 
         brokerList.removeAllItems();
         brokerList.addItem("Select Broker/Agent...");
+        brokerList.addItem("Add New Broker/Agent...");
 
         try {
             records.first();
@@ -204,7 +208,7 @@ public class BrokerPage extends WizardPage {
                 //                MessageBox.showError(ex, "Database Access");
             }
         }
-        brokerList.setName(""); // NOI18N
+        brokerList.setName("brokerList"); // NOI18N
         brokerList.addItemListener(new java.awt.event.ItemListener() {
             public void itemStateChanged(java.awt.event.ItemEvent evt) {
                 selectedBrokerChanged(evt);
@@ -220,6 +224,8 @@ public class BrokerPage extends WizardPage {
         phoneField.setName("Phone"); // NOI18N
 
         jLabel3.setText("Fax:");
+        
+        faxField.setName("Fax");
 
         jLabel4.setText("Email:");
 
@@ -280,7 +286,24 @@ public class BrokerPage extends WizardPage {
         );
     }// </editor-fold>                        
 
-    private void selectedBrokerChanged(java.awt.event.ItemEvent evt) {                                       
+    private void selectedBrokerChanged(java.awt.event.ItemEvent evt) {          
+        // Before doing anything else, we need to see if the user wants to add
+        //+ a new broker/agent to the database.
+        if ( brokerList.getSelectedItem().toString().equalsIgnoreCase(
+                "add new broker/agent...") ) {
+            NewBrokerDlg dlg = new NewBrokerDlg(null, true);
+            dlg.pack();
+            dlg.setVisible(true);
+            
+            // Make sure the user did not cancel the dialog.
+            if ( !dlg.isCancelled() ) {
+                String newBroker = dlg.getBroker().getContact() + " (" +
+                        String.valueOf(dlg.getBroker().getId()) + ")";
+                brokerList.addItem(newBroker);
+                brokerList.setSelectedItem(newBroker);
+            }
+        }
+        
         // In order to get the selected broker, we need to loop through the
         //+ records to find which record has the selected ID number.
         if ( !brokerList.getSelectedItem().toString().equalsIgnoreCase(
