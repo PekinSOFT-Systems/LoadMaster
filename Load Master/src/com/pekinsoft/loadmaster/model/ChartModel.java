0213fa6001f8d6dd92bbe3466b0a79cabe0d90e2
Sean Carrick <PekinSOFT@outlook.com>
2020-10-11 19:55:17 -0500

    Chart of Accounts Created
    Since we have gotten the Book New Load Wizard working properly, we need to start working on the accounting side of the project. The reason being that we need to have an Accounts Receivable account into which we can place the gross pay from the booked load.
    
    For this purpose, I have created a default chart of accounts to be used with the system. We may add the ability to add a custom chart of accounts to the system at a later version. For now, the default chart of accounts is valid for every one-truck owner/operator and breaks expenses down sufficiently. I have also added five (5) asset accounts: 
    
    * Checking Account
    * Savings Account
    * Retirement Savings Account
    * Fuel Card
    * Accounts Receivable
    
    Furthermore, I have added one (1) liability account, Accounts Payable. These twenty-four (24) accounts will suffice for 99+% of our target market for this version of Load Master.
    
    ***NOTE***: I will not merge this branch until the Wizard branch is merged to the master.
    
    -SC
    Signed-off-by:Sean Carrick <PekinSOFT@outlook.com>

---
+++ Load Master/src/com/pekinsoft/loadmaster/model/ChartModel.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2020 PekinSOFT Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * *****************************************************************************
+ * *****************************************************************************
+ *  Project    :   Load_Master
+ *  Class      :   ChartModel.java
+ *  Author     :   Sean Carrick
+ *  Created    :   Oct 11, 2020 @ 7:20:56 PM
+ *  Modified   :   Oct 11, 2020
+ *  
+ *  Purpose:
+ *  
+ *  Revision History:
+ *  
+ *  WHEN          BY                  REASON
+ *  ------------  ------------------- ------------------------------------------
+ *  Oct 11, 2020  Sean Carrick        Initial creation.
+ * *****************************************************************************
+ */
+
+package com.pekinsoft.loadmaster.model;
+
+/**
+ *
+ * @author Sean Carrick &lt;sean at pekinsoft dot com&gt;
+ * 
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+public class ChartModel {
+    //<editor-fold defaultstate="collapsed" desc="Public Static Constants">
+    
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Private Member Fields">
+    private long number;
+    private String name;
+    private String description;
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Static Initializer">
+    static {
+        
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Intstance Initializer">
+    {
+        
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Constructor(s)">
+    public ChartModel () {
+        
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Public Static Methods">
+    
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Public Instance Methods">
+    public long getNumber() {
+        return number;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Private Instance Methods">
+    
+    //</editor-fold>
+
+
+}
