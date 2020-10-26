c2d6a79554b99caa3d9855b6bc30a418876156a7
Jiri Kovalsky <jiri.kovalsky@oracle.com>
2020-10-26 18:29:43 +0100

    Increased sleep time before existing application to let AWTEventQueue finish all pending actions.

--- Load Master/test/com/pekinsoft/loadmaster/tests/MainWindowTest.java
+++ Load Master/test/com/pekinsoft/loadmaster/tests/MainWindowTest.java
@@ -61,7 +61,7 @@ public class MainWindowTest {
 
     @AfterClass
     public static void tearDownClass() {
-        Tools.sleep(1000);
+        Tools.sleep(3000);
         mainWindowOperator.callExit();
         Tools.pushButtonInDialog("Confirm Close", "Yes");
     }
